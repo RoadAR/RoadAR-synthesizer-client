d588254002c14130fd1f3807abfc0f5caa4faf0c
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2015-09-22 12:11:43 +0300

    Namespaces fix

--- lib/RoadAR/synthesizer/client/repositories/job_repository.rb
+++ lib/RoadAR/synthesizer/client/repositories/job_repository.rb
@@ -4,54 +4,56 @@ require 'RoadAR/synthesizer/client/entities/job'
 
 module RoadAR
   module Synthesizer
-    module Repositories
-      class JobRepository
-        include Singleton
+    module Client
+      module Repositories
+        class JobRepository
+          include Singleton
 
-        class << self
-          [:all, :create, :find].each do |method|
-            define_method(method) do |*args|
-              instance.send(method, *args)
+          class << self
+            [:all, :create, :find].each do |method|
+              define_method(method) do |*args|
+                instance.send(method, *args)
+              end
             end
           end
-        end
 
-        def all
-          get("#{base_url}/jobs.json").map do |job_json|
-            wrap(job_json)
+          def all
+            get("#{base_url}/jobs.json").map do |job_json|
+              wrap(job_json)
+            end
           end
+
+          def create(params)
+            wrap(post(base_url + '/jobs.json', params))
+          end
+
+          def find(id)
+            wrap(get("#{base_url}/jobs/#{id}.json"))
+          end
+
+          private
+
+          def wrap(entity)
+            RoadAR::Synthesizer::Client::Entities::Job.new(entity)
+          end
+
+          def base_url
+            @base_url ||= RoadAR::Synthesizer::Config.instance.url
+          end
+
+          def token
+            @token ||= RoadAR::Synthesizer::Config.instance.token
+          end
+
+          def get(url)
+            HTTParty.get(url, query: { token: token })
+          end
+
+          def post(url, **params)
+            HTTParty.post(url, body: { job: params }, query: { token: token })
+          end
+
         end
-
-        def create(params)
-          wrap(post(base_url + '/jobs.json', params))
-        end
-
-        def find(id)
-          wrap(get("#{base_url}/jobs/#{id}.json"))
-        end
-
-        private
-
-        def wrap(entity)
-          RoadAR::Synthesizer::Client::Entities::Job.new(entity)
-        end
-
-        def base_url
-          @base_url ||= RoadAR::Synthesizer::Config.instance.url
-        end
-
-        def token
-          @token ||= RoadAR::Synthesizer::Config.instance.token
-        end
-
-        def get(url)
-          HTTParty.get(url, query: { token: token })
-        end
-
-        def post(url, **params)
-          HTTParty.post(url, body: { job: params }, query: { token: token })
-        end
-
       end
     end
   end
